44cf64bb16ed4782bc595b873eeda3e6d3adb73a
avitsara <avitsarajary@yahoo.com>
2021-02-21 22:34:07 +0100

    ajout de style pour les composants

--- quizz/src/components/QuestionsReponses.js
+++ quizz/src/components/QuestionsReponses.js
@@ -10,7 +10,18 @@ class QuestionsReponses extends React.Component {
   render(){
     return(
       <Fragment>
-      <h1> Questions Réponses </h1>
+          <div className="container">
+            <div className="questions">
+                  <h1> Qu'est-ce qu'un Objet ?  </h1>
+                    <button className="reponses"> Une représentation d'une chose matérielle ou immatérielle  </button>
+                    <button className="reponses"> Un objet du monde réel  </button>
+                    <button className="reponses"> Une représentation d'un objet du monde réel </button>
+                    <button className="reponses">  Une représentation d'un objet du monde réel </button>
+            </div>
+
+
+          </div>
+
       </Fragment>
     )
   }
