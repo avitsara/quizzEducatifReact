bd87b8e95514126d97dbab2b9327536550825692
avitsara <avitsarajary@yahoo.com>
2021-02-22 00:31:53 +0100

    Modification du composant QuestionsReponses

--- quizz/src/components/QuestionsReponses.js
+++ quizz/src/components/QuestionsReponses.js
@@ -4,19 +4,43 @@ import ReactDOM from 'react-dom';
 import { Fragment } from 'react';
 
 /*================  COMPOSANT REPRÉSENTANT LES QUESTIONS ET LES RÉPONSES =================*/
-class QuestionsReponses extends React.Component {
+function QuestionsReponses(){
 
+      const questions = [
+        {
+          question : "Quelle est la capitale de l'Italie",
+          reponses : [
+              { reponse: "Rome" , correct : true},
+              { reponse: "Milan" , correct : false},
+              { reponse : "Venise" , correct : false},
+              { reponse : "Vérone" , correct : false},
+          ],
 
-  render(){
+        },
+        {
+          question : "Quelle est la capitale de l'Allemagne",
+          reponses : [
+              { reponse: "Hambourg" , correct : false},
+              { reponse: "Berlin" , correct : true},
+              { reponse : "Munich" , correct : false},
+              { reponse : "Cologne" , correct : false},
+          ],
+        }
+      ]
+
     return(
       <Fragment>
           <div className="container">
             <div className="questions">
-                  <h1> Qu'est-ce qu'un Objet ?  </h1>
-                    <button className="reponses"> Une représentation d'une chose matérielle ou immatérielle  </button>
-                    <button className="reponses"> Un objet du monde réel  </button>
-                    <button className="reponses"> Une représentation d'un objet du monde réel </button>
-                    <button className="reponses">  Une représentation d'un objet du monde réel </button>
+                  <h1> { questions[0].question}</h1>
+                  <div className="questions">
+                    <button className="reponses"> {questions[0].reponses[0].reponse} </button>
+                    <button className="reponses"> {questions[0].reponses[1].reponse} </button>
+                    <button className="reponses"> {questions[0].reponses[2].reponse} </button>
+                    <button className="reponses"> {questions[0].reponses[3].reponse} </button>
+
+
+                  </div>
             </div>
 
 
@@ -24,7 +48,6 @@ class QuestionsReponses extends React.Component {
 
       </Fragment>
     )
-  }
 }
 /*====  EXPORTATION PAR DÉFAUT ===*/
 export default QuestionsReponses;
