649a3fa1bd84c99c11cbc1c4f0e3fcd5f9dd88e1
avitsara <avitsarajary@yahoo.com>
2021-02-22 09:18:46 +0100

    modification des questions/réponses

--- quizz/src/components/QuestionsReponses.js
+++ quizz/src/components/QuestionsReponses.js
@@ -8,7 +8,7 @@ function QuestionsReponses(){
 
       const questions = [
         {
-          question : "Quelle est la capitale de l'Italie",
+          question : "Quelle est la capitale de l'Italie ? ",
           reponses : [
               { reponse: "Rome" , correct : true},
               { reponse: "Milan" , correct : false},
@@ -18,12 +18,22 @@ function QuestionsReponses(){
 
         },
         {
-          question : "Quelle est la capitale de l'Allemagne",
+          question : "Quelle est la capitale de l'Allemagne ? ",
           reponses : [
               { reponse: "Hambourg" , correct : false},
               { reponse: "Berlin" , correct : true},
               { reponse : "Munich" , correct : false},
               { reponse : "Cologne" , correct : false},
+          ],
+        },
+
+        {
+          question : "Quelle est la capitale du Luxembourg ? ",
+          reponses : [
+              { reponse: "Hambourg" , correct : false},
+              { reponse: "Dudelange" , correct : false},
+              { reponse : "Vianden" , correct : false},
+              { reponse : "Luxembourg" , correct : true},
           ],
         }
       ]
@@ -32,13 +42,11 @@ function QuestionsReponses(){
       <Fragment>
           <div className="container">
             <div className="questions">
-                  <h1> { questions[0].question}</h1>
-                  <div className="questions">
-                    <button className="reponses"> {questions[0].reponses[0].reponse} </button>
-                    <button className="reponses"> {questions[0].reponses[1].reponse} </button>
-                    <button className="reponses"> {questions[0].reponses[2].reponse} </button>
-                    <button className="reponses"> {questions[0].reponses[3].reponse} </button>
-
+                  <h1> Bienvenue dans ce Quizz éducatif !!!  </h1>
+                  <span>Question 1</span>/{questions.length}
+                  <h2 className="question"> { questions[0].question}  </h2>
+                  <div className="reponsesContainer">
+                    {questions[0].reponses.map((reponse) => <button className="reponses">  {reponse.reponse} </button>)}
 
                   </div>
             </div>
